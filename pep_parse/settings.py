83d88d9fd04900b4a34d3187ec0200c47809f2d2
Иван Сиснёв <ivan.sisnev@gmail.com>
2023-09-05 22:40:14 +0300

    стиль

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -9,12 +9,9 @@ NEWSPIDER_MODULE = 'pep_parse.spiders'
 
 ROBOTSTXT_OBEY = True
 
-ITEM_PIPELINES = {
-   'pep_parse.pipelines.PepParsePipeline': 300,
-}
+ITEM_PIPELINES = {'pep_parse.pipelines.PepParsePipeline': 300, }
 
-FEEDS = {
-    f'results/pep_%(time)s.csv': {
+FEEDS = {'results/pep_%(time)s.csv': {
         'format': 'csv',
         'fields': ['number', 'name', 'status'],
         'overwrite': True
--- pep_parse/utils.py
+++ pep_parse/utils.py
@@ -1,8 +1,10 @@
 """
 Utils проекта.
 """
+import logging
 
-def scrape(obj, xpath: str =None, css: str =None, attrib: str =None):
+
+def scrape(obj, xpath: str = None, css: str = None, attrib: str = None):
     """
     Забирает данные из объекта response или Selector c отработкой ошибки.
     :param obj: объект парсинга.
@@ -11,22 +13,20 @@ def scrape(obj, xpath: str =None, css: str =None, attrib: str =None):
     :param attrib: строка для метода attrib
     :return: объект Selector
     """
-    try:
-        result = None
-        if xpath:
-            result = obj.xpath(xpath)
-        elif css:
-            result = obj.css(css)
-        elif attrib:
-            result = obj.attrib[attrib]
-        if result:
-            return result
-    except:
-        pass
+    result = None
+    if xpath:
+        result = obj.xpath(xpath)
+    elif css:
+        result = obj.css(css)
+    elif attrib:
+        result = obj.attrib[attrib]
 
-        # except IOError as exc:
-        #     error_msg: str = ('Не удалось записать данные в файл.')
-        #     logging.exception(msg=error_msg, stack_info=True)
-        #     raise SystemExit(error_msg) from exc
+    if not result:
+        error_msg: str = (f'В объекте {obj} не найден тег или аттрибут:'
+                          f'\nxpath {xpath}'
+                          f'\ncss {css}'
+                          f'\nattrib {attrib}')
+        logging.critical(msg=error_msg, stack_info=True)
+        raise SystemExit(error_msg)
 
-
+    return result
